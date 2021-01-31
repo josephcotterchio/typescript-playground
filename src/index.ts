58b0c1d8516a3cfb1a9d610696b5ab1e06a1991d
joecotterchio <joecotterchio@gmail.com>
2021-01-30 20:11:08 -0500

    first pass

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// type Pet = {
+//   name: string;
+//   type: string;
+// };
+
+// const dogBuilder = (name: string): Pet => {
+//   let jd: Pet = {
+//     name: "J.D.",
+//     type: "dog",
+//   };
+
+//   return jd;
+// };
+
+// const dogBuilderJs = (name) => {
+//   let jd = {
+//     name: "J.D.",
+//     type: "dog",
+//   };
+
+//   return jd;
+// };
+
+class Pet {
+  name: string;
+  type: string;
+}
+
+const pet = new Pet();
+pet.name = "JD";
+pet.type = "dog";
+
+
